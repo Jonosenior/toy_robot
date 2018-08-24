44fb74a176c20be9f8ea402e0ff3118d3d37f4a0
jonathan senior <jonosenior@gmail.com>
2018-08-24 12:17:00 +0200

    Add basic tests for facing direction and failing test for turn left

--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,11 +1,22 @@
-require File.expand_path("../../lib/toy_robot", __FILE__)
+require File.expand_path('../../lib/toy_robot', __FILE__)
 
 RSpec.describe ToyRobot::Robot do
-  context 'when initialized' do
-    subject(:robot) { ToyRobot::Robot.new([0,0], "NORTH") }
+  subject(:robot) { ToyRobot::Robot.new([0,0], 'NORTH') }
 
+  context 'when initialized' do
     it 'returns the location' do
       expect(robot.location).to eq([0,0])
+    end
+
+    it 'returns the facing direction' do
+      expect(robot.facing).to eq('NORTH')
+    end
+  end
+
+  context 'when told to turn left' do
+    it 'returns the new facing direction' do
+      robot.turn("left")
+      expect(robot.facing).to eq('WEST')
     end
   end
 end
