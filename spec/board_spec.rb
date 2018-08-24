587edb195f3cfa9d3cd981e0abb90ecaacc6ac43
jonathan senior <jonosenior@gmail.com>
2018-08-24 13:11:17 +0200

    Add further tests for valid_location?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,6 +8,25 @@ RSpec.describe ToyRobot::Board do
       it 'returns negative x-axis as invalid' do
         expect(board.valid_location?([-1,0])).to be_falsey
       end
+
+      it 'returns negative y-axis as invalid' do
+        expect(board.valid_location?([0,-1])).to be_falsey
+      end
+
+      it 'returns too-large x-axis as invalid' do
+        expect(board.valid_location?([6,1])).to be_falsey
+      end
+
+      it 'returns too-large y-axis as invalid' do
+        expect(board.valid_location?([2,6])).to be_falsey
+      end
+
+      it 'returns valid location as valid' do
+        expect(board.valid_location?([0,0])).to be_truthy
+      end
+
+
+
     end
   end
 
