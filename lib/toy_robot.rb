3bc6b7a87d87d4a36c51a3750f3e3dddc7742c21
jonathan senior <jonosenior@gmail.com>
2018-08-24 12:06:16 +0200

    Add first working test for robot

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -2,6 +2,8 @@ require "toy_robot/version"
 
 module ToyRobot
   class Robot
+    attr_reader :location, :facing
+
     def initialize(location, facing)
       @location, @facing = location, facing
     end
