eab8e12089b456558ba07cd20b23cb126bdcf7df
jonathan senior <jonosenior@gmail.com>
2018-08-27 16:58:48 +0200

    Refactor file structure

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -1,77 +1,9 @@
 require_relative '../lib/toy_robot/table'
 require_relative '../lib/toy_robot/robot'
+require_relative '../lib/toy_robot/application'
 require 'pry'
 
 module ToyRobot
-  class Application
-
-    def initialize
-      @table = ToyRobot::Table.new([5,5])
-    end
-
-    def new_round
-      loop do
-        input = elicit_input
-        input = convert_input(input)
-        # binding.pry
-        if !is_input_valid?(input)
-          puts 'Please give a valid instruction!'
-          redo
-        end
-        make_move(input)
-      end
-    end
-
-    def is_input_valid?(input)
-      command = input[:command]
-      possible_commands = ["PLACE", "MOVE", "LEFT", "RIGHT", "REPORT"]
-      return false unless possible_commands.include?(command)
-      if command == "PLACE"
-        possible_directions = ['NORTH', 'EAST', 'SOUTH', 'WEST']
-        return false unless possible_directions.include?(input[:facing])
-        return false if !@table.valid_location?(input[:location])
-        return true
-      end
-      return false if !@robot && command != "PLACE"
-      return false if command == "MOVE" && !@table.valid_move?(@robot.location, @robot.facing)
-      true
-    end
-
-    def make_move(input)
-      case input[:command]
-        when "PLACE"
-          create_robot(input)
-        when "MOVE"
-          new_location = @table.location_after_move(@robot.location, @robot.facing)
-          @robot.move_to(new_location)
-        when "LEFT"
-          @robot.turn("LEFT")
-        when "RIGHT"
-          @robot.turn("RIGHT")
-        when "REPORT"
-          puts "\n#{@robot.location[0]},#{@robot.location[1]},#{@robot.facing}\n"
-      end
-    end
-
-    def convert_input(input)
-      input = input.split(/[\s^,]+/)
-      input = { command: input[0].upcase,
-                location: [input[1].to_i, input[2].to_i],
-                facing: input[3]
-              }
-    end
-
-    def create_robot(input)
-      location = input[:location]
-      facing = input[:facing]
-      @robot = ToyRobot::Robot.new({location: location, facing: facing})
-    end
-
-    def elicit_input
-      puts "\n\nChoose:\nPLACE X,Y,F (e.g. 'PLACE 0,3,NORTH')\nMOVE\nLEFT\nRIGHT\nREPORT\n\n"
-      gets.chomp
-    end
-  end
 end
 
 app = ToyRobot::Application.new
