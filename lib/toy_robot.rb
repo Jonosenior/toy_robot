ebd7d4131e6c85e9a8fcd51edd3c2f36eb5333b5
jonathan senior <jonosenior@gmail.com>
2018-08-27 10:13:19 +0200

    Refactor application#input method

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -10,22 +10,27 @@ module ToyRobot
     end
 
     def new_round
-      location_and_facing = elicit_input
-
-
-
-
+      loop do
+        input = elicit_input
+        input = convert_input(input)
+        binding.pry
+        if !@board.valid_location?(input[:location])
+          puts 'Not a valid location!'
+          redo
+        end
+        @robot.new(input[:location], input[:facing])
+      end
     end
 
     def convert_input(input)
-      input = input.split(",")
-      location_and_facing = { location => [input[0], input[1]],
-                              facing => input[2]
-                            }
+      input = input.split(/[\s^,]+/)
+      input = { command: input[0],
+                location: [input[1], input[2]],
+                facing: input[3]
+              }
     end
 
     def elicit_input
-      # binding.pry
       puts "Choose:\nPLACE X,Y,F (e.g. 'PLACE 0,3,NORTH')\nMOVE\nLEFT\nRIGHT\nREPORT"
       gets.chomp
     end
