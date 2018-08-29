a24300744984733d1f0a9e4e89c5523fdb6c9aa5
jonathan senior <jonosenior@gmail.com>
2018-08-29 13:54:21 +0200

    Add comments to robot class

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,5 +1,8 @@
 
 module ToyRobot
+
+  # Stores the current location and facing direction of
+  # the robot, and can edit either.
   class Robot
     attr_reader :location, :facing
 
@@ -8,16 +11,22 @@ module ToyRobot
       @facing = input.fetch(:facing)
     end
 
+    # Changes location based on array input of form
+    # [x-coordinate, y-coordinate].
     def move_to(location)
       @location = location
     end
 
+    # Changes facing direction based on string input of either
+    # 'LEFT' or 'RIGHT'.
     def turn(left_or_right)
       turn_left = { 'NORTH' => 'WEST', 'WEST' => 'SOUTH', 'SOUTH' => 'EAST', 'EAST' => 'NORTH' }
       turn_right = { 'NORTH' => 'EAST', 'EAST' => 'SOUTH', 'SOUTH' => 'WEST', 'WEST' => 'NORTH' }
       @facing = (left_or_right == 'LEFT') ? turn_left[@facing] : turn_right[@facing]
     end
 
+    # Converts location and facing direction into a
+    # convenient hash. 
     def location_and_facing
       { location: @location, facing: @facing }
     end
